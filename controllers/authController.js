c47e49899fd78e7f910a00a059294914b1ba0fca
Rahul rathore <rathorrahul0112@gmail.com>
2024-10-13 15:38:15 +0530

    file updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,7 +65,7 @@ export const loginController = async(req,res) =>{
     try{
         const {email,password} = req.body
         //validation
-        if(!email || password){
+        if(!email || !password){
             return res.status(404).send({
                 success:false,
                 message:'Invalid email or password'
@@ -87,7 +87,7 @@ export const loginController = async(req,res) =>{
         }
 
         // token
-        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        const token = await JWT.sign({ _id:user._id }, process.env.JWT_SECRET, {
             expiresIn: "7d",
         });
 
